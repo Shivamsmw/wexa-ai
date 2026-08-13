0c76b5443ceff63cf2f18d38c781c5a35fbfe88d
Shivam Agrawal <shivamsmw.iimt@gmail.com>
2026-08-13 10:16:15 +0000

    [HOSTING ]

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:4000'
+      '/api': 'https://cognodb-assignment-shivam-backend.onrender.com'
     }
   }
 });
